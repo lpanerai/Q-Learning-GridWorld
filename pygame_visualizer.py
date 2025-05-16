99f8c1b9dbba0f6b7de28554e81400d656152f17
lpanerai <l.panerai@campus.unimib.it>
2025-05-16 12:22:11 +0200

    deleted the double class

--- pygame_visualizer.py
+++ pygame_visualizer.py
@@ -4,55 +4,6 @@ from GridWorld import GridWorldEnv
 from QlearningAgent import QLearningAgent
 from  utility import run_simulation
 
-class PygameGridWorld:
-    CELL_SIZE = 80
-    MARGIN = 2
-    FPS = 10
-
-    def __init__(self, env):
-        pygame.init()
-        self.env = env
-        self.width = env.width * (self.CELL_SIZE + self.MARGIN) + self.MARGIN
-        self.height = env.height * (self.CELL_SIZE + self.MARGIN) + self.MARGIN
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption("GridWorld Q-Learning")
-        self.clock = pygame.time.Clock()
-        self.font = pygame.font.SysFont(None, 24)
-
-    def draw_grid(self, agent_pos):
-        self.screen.fill((50, 50, 50))
-        for x in range(self.env.width):
-            for y in range(self.env.height):
-                rect = pygame.Rect(
-                    x * (self.CELL_SIZE + self.MARGIN) + self.MARGIN,
-                    y * (self.CELL_SIZE + self.MARGIN) + self.MARGIN,
-                    self.CELL_SIZE,
-                    self.CELL_SIZE
-                )
-                color = (255, 255, 255)
-                if (x, y) in self.env.obstacles:
-                    color = (0, 0, 0)
-                elif (x, y) == self.env.start:
-                    color = (0, 255, 0)
-                elif (x, y) == self.env.goal:
-                    color = (255, 0, 0)
-                pygame.draw.rect(self.screen, color, rect)
-
-        # draw agent
-        ax, ay = agent_pos
-        agent_rect = pygame.Rect(
-            ax * (self.CELL_SIZE + self.MARGIN) + self.MARGIN,
-            ay * (self.CELL_SIZE + self.MARGIN) + self.MARGIN,
-            self.CELL_SIZE,
-            self.CELL_SIZE
-        )
-        pygame.draw.circle(
-            self.screen,
-            (0, 0, 255),
-            agent_rect.center,
-            self.CELL_SIZE // 3
-        )
-
 import pygame
 import sys
 from GridWorld import GridWorldEnv
